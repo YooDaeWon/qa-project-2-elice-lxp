f4b1c6ecc0b36adf35d10931194e83126654481c
hongbotwoo <gonggae1@naver.com>
2026-09-03 15:21:06 +0900

    timeout 테스트 timeout 인자 제거

--- tests/e2euiux/test_exam_timeout.py
+++ tests/e2euiux/test_exam_timeout.py
@@ -84,4 +84,4 @@ def test_verify_timeout_modal(
     e2e_page.clock.install()
     exam_page = _open_timeout_exam_page(e2e_page, credentials)
     e2e_page.clock.fast_forward("04:58")
-    exam_page.verify_timeout_modal(timeout=10_000)
+    exam_page.verify_timeout_modal()
